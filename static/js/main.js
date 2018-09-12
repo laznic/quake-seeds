b18bbe7e006c8b0707b7ada0594cc4c51d8408bb
Niklas Lepisto <niklas.lepisto@idean.com>
2018-09-12 16:30:57 +0300

    Initial functionalities to add players manually

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,18 @@ function onKeyUp(e) {
   }
 }
 
+function onKeyUpName(e) {
+  if (e.target.value.length) {
+    const allNameInputs = Array.from(document.querySelectorAll('.name'))
+    const currentNameIndex = allNameInputs.findIndex(element => element === e.target)
+
+    if (currentNameIndex + 2 > allNameInputs.length) {
+      const playerContainer = document.querySelector('.players')
+      playerContainer.insertAdjacentHTML('beforeend', e.target.outerHTML)
+    }
+  }
+}
+
 function goToSeeds() {
   const inputElement = document.querySelector('.id')
   const id = inputElement.value
